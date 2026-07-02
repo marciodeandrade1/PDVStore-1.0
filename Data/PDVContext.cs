a17840ebe64fa008a2eb9a4a362254712ef3b247
Marcio de Andrade <marcio@requestdev.net>
2026-07-02 09:38:22 -0400

    Criação ConnectionHelper para connection string e PDVContext

---
+++ Data/PDVContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PDVStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDVStore.Data
+{
+    public class PDVContext : DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Venda> Vendas { get; set; }
+        public DbSet<ItemVenda> ItensVenda { get; set; }
+        public DbSet<UsuarioCaixa> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Use Helpers.ConnectionHelper para connection string
+                optionsBuilder.UseSqlServer(Helpers.ConnectionHelper.GetConnectionString());
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ItemVenda>()
+                .HasOne(i => i.Venda)
+                .WithMany(v => v.Itens)
+                .HasForeignKey(i => i.IdVenda);
+
+            modelBuilder.Entity<ItemVenda>()
+                .HasOne(i => i.Produto)
+                .WithMany()
+                .HasForeignKey(i => i.IdProduto);
+
+            modelBuilder.Entity<Produto>()
+                .Property(p => p.EstoqueMinimo)
+                .HasDefaultValue(10);
+        }
+    }
+}
---
+++ Helpers/ConnectionHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PDVStore.Helpers
+{
+    public class ConnectionHelper
+    {
+        public static string GetConnectionString()
+        {
+            // Return your connection string here
+            return @"Server=(localdb)\\mssqllocaldb;Database=PDV_StoreDB;Trusted_Connection=True;";
+        }
+    }
+}

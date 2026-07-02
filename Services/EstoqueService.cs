e4e4fb54d1914e1878ade13826f1ed22ec385e20
Marcio de Andrade <marcio@requestdev.net>
2026-07-02 10:47:42 -0400

    EstoqueService

---
+++ Services/EstoqueService.cs
@@ -0,0 +1,33 @@
+﻿using PDVStore.Data;
+using PDVStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDVStore.Services
+{
+    public class EstoqueService
+    {
+        private readonly PDVContext _context;
+
+        public EstoqueService(PDVContext context)
+        {
+            _context = context;
+        }
+
+        public void AdicionarProduto(Produto produto)
+        {
+            _context.Produtos.Add(produto);
+            _context.SaveChanges();
+        }
+
+        public List<Produto> GetProdutosBaixoEstoque()
+        {
+            return _context.Produtos.Where(p => p.QuantidadeEstoque < p.EstoqueMinimo).ToList();
+        }
+
+        // Outros métodos: Editar, Remover, etc.
+    }
+}

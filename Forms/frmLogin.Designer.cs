9e517c5243eb48fb8e516ab77c10844a9ee0304f
Marcio de Andrade <marcio@requestdev.net>
2026-07-02 08:45:34 -0400

    frmLogin

---
+++ Forms/frmLogin.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PDVStore.Forms
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnLogin = new Button();
+            lblUsuario = new Label();
+            lblSenha = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(208, 119);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 0;
+            btnLogin.Text = "Acessar";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // lblUsuario
+            // 
+            lblUsuario.AutoSize = true;
+            lblUsuario.Location = new Point(26, 24);
+            lblUsuario.Name = "lblUsuario";
+            lblUsuario.Size = new Size(80, 20);
+            lblUsuario.TabIndex = 1;
+            lblUsuario.Text = "Usuário(a):";
+            // 
+            // lblSenha
+            // 
+            lblSenha.AutoSize = true;
+            lblSenha.Location = new Point(30, 71);
+            lblSenha.Name = "lblSenha";
+            lblSenha.Size = new Size(52, 20);
+            lblSenha.TabIndex = 2;
+            lblSenha.Text = "Senha:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(112, 21);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(190, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(112, 71);
+            textBox2.Name = "textBox2";
+            textBox2.PasswordChar = '*';
+            textBox2.Size = new Size(190, 27);
+            textBox2.TabIndex = 3;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(354, 190);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(lblSenha);
+            Controls.Add(lblUsuario);
+            Controls.Add(btnLogin);
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "PDV Store - Login";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button btnLogin;
+        private Label lblUsuario;
+        private Label lblSenha;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,7 +1,12 @@
+using PDVStore.Forms;
+//using PowerArgs;
+
 namespace PDVStore
 {
     internal static class Program
     {
+        public static object WinFormsApp { get; private set; }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -11,7 +16,23 @@ namespace PDVStore
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+
+            // Program.cs (.NET 8 WinForms com Host)
+         //   var builder = WinFormsApp. .CreateBuilder(Args);
+
+            // Connection string centralizada
+            string conn = @"Server=(localdb)\mssqllocaldb;Database=PDV_LojaDB;Trusted_Connection=True;";
+
+            //builder.Services.AddDbContext<PdvContext>(opt => opt.UseSqlServer(conn));
+            //builder.Services.AddTransient<VendaService>();
+            //builder.Services.AddTransient<EstoqueService>();
+            //builder.Services.AddTransient<RelatorioService>();
+            //builder.Services.AddTransient<PagamentoIntegrator>();
+            //builder.Services.AddSingleton<DashboardViewModel>();
+
+            //var app = builder.Build();
+           // app.Run<frmLogin>();
+            Application.Run(new frmLogin());
         }
     }
 }

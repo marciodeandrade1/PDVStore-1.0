e1ca6c5dbb0bf56aecb8bf8377c4ed2eacb72985
Marcio de Andrade <marcio@requestdev.net>
2026-07-07 12:41:01 -0400

    Model MovimentacaoEstoque

---
+++ Models/MovimentacaoEstoque.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDVStore.Models
+{
+    public class MovimentacaoEstoque
+    {
+        public int Id { get; set; }
+        public int ProdutoId { get; set; }
+        public string Tipo { get; set; } = string.Empty; // "Entrada" ou "Saída"
+        public int Quantidade { get; set; }
+        public decimal? PrecoUnitario { get; set; }
+        public DateTime DataMovimentacao { get; set; } = DateTime.UtcNow;
+        public int UsuarioId { get; set; }
+        public string? Motivo { get; set; }
+        public int? ReferenciaVendaId { get; set; } // Para saídas automáticas de venda
+
+        // Navegação
+        public Produto? Produto { get; set; }
+        public UsuarioCaixa? Usuario { get; set; }
+    }
+}

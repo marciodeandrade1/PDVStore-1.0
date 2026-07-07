82f3d451cf5f8c37b4dbfedec6307208b6c3d34a
Marcio de Andrade <marcio@requestdev.net>
2026-07-07 12:49:44 -0400

    Migration MovEstoque

---
+++ Migrations/20260707164906_MovEstoque.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PDVStore.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovEstoque : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MovimentacoesEstoque",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProdutoId = table.Column<int>(type: "int", nullable: false),
+                    Tipo = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Quantidade = table.Column<int>(type: "int", nullable: false),
+                    PrecoUnitario = table.Column<decimal>(type: "decimal(18,2)", nullable: true),
+                    DataMovimentacao = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UsuarioId = table.Column<int>(type: "int", nullable: false),
+                    Motivo = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ReferenciaVendaId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MovimentacoesEstoque", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MovimentacoesEstoque_Produtos_ProdutoId",
+                        column: x => x.ProdutoId,
+                        principalTable: "Produtos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MovimentacoesEstoque_UsuarioCaixa_UsuarioId",
+                        column: x => x.UsuarioId,
+                        principalTable: "UsuarioCaixa",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovimentacoesEstoque_ProdutoId",
+                table: "MovimentacoesEstoque",
+                column: "ProdutoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MovimentacoesEstoque_UsuarioId",
+                table: "MovimentacoesEstoque",
+                column: "UsuarioId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MovimentacoesEstoque");
+        }
+    }
+}
